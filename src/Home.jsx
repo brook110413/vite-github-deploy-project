ab3eb052af89e81d7368de59b94ecf6dcb617cae
Jason Hung <jason.hung@tomofun.com>
2023-03-24 11:50:00 +0800

    Update

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import process from 'process';
 
-const Home = (props) => {
-  return <div>Home</div>;
+const Home = () => {
+  console.log('process', process.env.NODE_ENV);
+
+  return <div>Home1</div>;
 };
 
 export default Home;
